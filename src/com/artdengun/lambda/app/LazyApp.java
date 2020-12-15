b0f349b08377605ad8cf9d37de7012a830633f8e
DeniGunawan <artdengun@gmail.com>
2020-12-15 17:16:20 +0700

    java-lambda

--- src/com/artdengun/lambda/app/LazyApp.java
+++ src/com/artdengun/lambda/app/LazyApp.java
@@ -1,2 +1,22 @@
-package com.artdengun.lambda.app;public class LazyApp {
+package com.artdengun.lambda.app;
+
+import java.util.function.Supplier;
+
+public class LazyApp {
+    public static void main(String[] args) {
+        testScore(40, () -> getName());
+    }
+
+    public static void testScore(int score, Supplier<String> name){
+        if(score > 80){
+            System.out.println("Selamat " + name.get() + ", Anda lulus");
+        }else {
+            System.out.println("COBA TAHUN DEPAN");
+        }
+    }
+
+    public static String getName(){
+        System.out.println("Method getName() dipanggil");
+        return "deni";
+    }
 }
--- src/com/artdengun/lambda/app/OptionalApp.java
+++ src/com/artdengun/lambda/app/OptionalApp.java
@@ -1,2 +1,36 @@
-package com.artdengun.lambda.app;public class OptionalApp {
+package com.artdengun.lambda.app;
+
+import java.util.Optional;
+
+public class OptionalApp {
+    public static void main(String[] args) {
+        sayHello("deni");
+        String name = null;
+        sayHello(name);
+    }
+
+    public static  void sayHello(String name){
+
+        String upperName = Optional.ofNullable(name)
+                .map(String::toUpperCase)
+                .orElse("Teman");
+
+        System.out.println("Hello " + name);
+
+//        Optional.ofNullable(name)
+//                .map(String::toUpperCase)
+//                .ifPresentOrElse(value -> System.out.println("Hello " + value),
+//                        () -> System.out.println("Hello")
+//                        );
+
+//        Optional<String> optionalName = Optional.ofNullable(name);
+//        Optional<String> optionalNameUpper = optionalName.map(value -> name.toUpperCase());
+//        optionalNameUpper.ifPresent(value -> System.out.println("Hello" + value));
+
+        // jika menggunakan check null
+//        if(name != null){
+//            String upperName = name.toUpperCase();
+//            System.out.println("Hello" + upperName);
+//        }
+    }
 }
